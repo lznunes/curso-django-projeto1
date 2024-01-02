163cacc3f0c3e85303f35f9c21ac96a956154af5
Luiz Antonio Nunes Neves <lznunes@gmail.com>
2024-01-02 13:08:33 -0300

    removida referencia de contato e sobre da pagina home

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
