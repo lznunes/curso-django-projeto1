d68dfbc755a399b584e1982b05c4a2452a46cb82
Luiz Antonio Nunes Neves <lznunes@gmail.com>
2024-01-02 22:20:43 -0300

    criação do cabeçãlho com css

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ def home(request):
     #return  render(request, 'global/home.html')
     #return  render(request, 'recipes/home.html', status=404)
 
-    return  render(request, 'recipes/home.html', status=404,context={
+    return  render(request, 'recipes/pages/home.html',context={
         'name': 'Luiz Nunes',
         } )
 
